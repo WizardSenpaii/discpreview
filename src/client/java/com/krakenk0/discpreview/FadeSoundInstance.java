322089b89210394e2dc9e405ae01a7abaab3443b
WizardSenpaii <88693341+WizardSenpaii@users.noreply.github.com>
2026-07-11 02:00:32 +0200

    Update FadeSoundInstance.java

--- src/client/java/com/krakenk0/discpreview/FadeSoundInstance.java
+++ src/client/java/com/krakenk0/discpreview/FadeSoundInstance.java
@@ -30,7 +30,7 @@ public class FadeSoundInstance extends AbstractTickableSoundInstance {
 
         this.attenuation = SoundInstance.Attenuation.NONE;
 
-        // Keep it attached to the player
+        
         this.x = 0;
         this.y = 0;
         this.z = 0;
